3a5d7cba467618eb6386639040c8b0daf327725c
Parker Brandt <parker.brandt101@gmail.com>
2022-04-28 21:59:04 -0500

    Initial commit

---
+++ Problem_1/find_longest_words_MRJob.py
@@ -0,0 +1,13 @@
+from mrjob.job import MRJob
+
+class MRLongestWord(MRJob):
+    
+    def mapper():
+        return
+
+    def reducer():
+        return    
+
+
+if __name__ == "__main__":
+    MRLongestWord.run() 
