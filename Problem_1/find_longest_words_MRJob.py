9e7ff2f23f9baa7617b354c61b9bfdae869394c7
Parker Brandt <parker.brandt101@gmail.com>
2022-04-30 13:15:49 -0500

    Wrote version that finds count of each word as just practicing MRJob

--- Problem_1/find_longest_words_MRJob.py
+++ Problem_1/find_longest_words_MRJob.py
@@ -1,12 +1,23 @@
 from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"[\w']+")
 
 class MRLongestWord(MRJob):
     
-    def mapper():
-        return
+    def mapper(self, _, line):
+        word_dictionary = {}
+        for word in WORD_RE.findall(line):
+            if word.lower() in word_dictionary:
+                word_dictionary[word.lower()] += 1
+            else:
+                word_dictionary[word.lower()] = 1
 
-    def reducer():
-        return    
+        for(key, local_value) in word_dictionary.items():
+            yield(key, local_value)
+
+    def reducer(self, word, counts):
+        yield(word, sum(counts)) 
 
 
 if __name__ == "__main__":
