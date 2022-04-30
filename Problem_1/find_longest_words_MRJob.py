5fd5b760b8bf282d1fb5f12bfb4e706cd554c332
Parker Brandt <parker.brandt101@gmail.com>
2022-04-30 13:45:22 -0500

    Wrote first version to find longest word for each letter - need to test

--- Problem_1/find_longest_words_MRJob.py
+++ Problem_1/find_longest_words_MRJob.py
@@ -6,18 +6,21 @@ WORD_RE = re.compile(r"[\w']+")
 class MRLongestWord(MRJob):
     
     def mapper(self, _, line):
-        word_dictionary = {}
+        longest_dictionary = {}
         for word in WORD_RE.findall(line):
-            if word.lower() in word_dictionary:
-                word_dictionary[word.lower()] += 1
+            if word.lower()[0] in longest_dictionary:
+                if len(word) > len(longest_dictionary[word.lower()[0]][0]):
+                    longest_dictionary[word.lower()[0]] = list(word.lower())
+                elif len(word) == len(longest_dictionary[word.lower()[0]][0]):
+                    longest_dictionary[word.lower()[0]].append(word.lower())
             else:
-                word_dictionary[word.lower()] = 1
+                longest_dictionary[word.lower()[0]] = list(word.lower())
 
-        for(key, local_value) in word_dictionary.items():
+        for(key, local_value) in longest_dictionary.items():
             yield(key, local_value)
 
-    def reducer(self, word, counts):
-        yield(word, sum(counts)) 
+    def reducer(self, letter, longest):
+        yield(letter, longest) 
 
 
 if __name__ == "__main__":
